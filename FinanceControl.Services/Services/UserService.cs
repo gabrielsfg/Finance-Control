f4f8bc801403355ea257153316ff5bc7bb433f9d
Gabrielsfg <gabriellfsg00@gmail.com>
2025-12-27 04:50:24 -0300

    Start of create account, login, authentication and authorization

---
+++ FinanceControl.Services/Services/UserService.cs
@@ -0,0 +1,44 @@
+﻿using FinanceControl.Data.Data;
+using FinanceControl.Domain.Entities;
+using FinanceControl.Domain.Interfaces.Service;
+using FinanceControl.Shared.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceControl.Services.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public UserService(ApplicationDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        public async Task<User?> RegisterUserAsync(CreateUserRequestDto requestDto)
+        {
+            if (await _context.Users.AnyAsync(u => u.Email == requestDto.Email))
+                return null;
+
+            var user = new User();
+            var hasedPassword = new PasswordHasher<User>().HashPassword(user, requestDto.Password);
+
+            user.Email = requestDto.Email;
+            user.Name = requestDto.Name;
+
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
---
+++ FinanceControl.WebApi/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using FinanceControl.Domain.Interfaces.Service;
+using FinanceControl.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceControl.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUserAsync(CreateUserRequestDto requestDto)
+        {
+            await _userService.RegisterUserAsync(requestDto);
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> UserLoginAsync()
+    }
+}
