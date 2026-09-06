49e36984dd06863d27e65ff6aec4bd780185e3ed
Gabrielsfg <gabriellfsg00@gmail.com>
2026-09-06 19:25:07 -0300

    Add missing migrations

---
+++ apps/api/FinanceControl.Data/Migrations/20260904033744_AddInvestmentYieldIndex.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceControl.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInvestmentYieldIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "YieldIndex",
+                table: "Investments",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "YieldIndex",
+                table: "Investments");
+        }
+    }
+}
